da583e736593f5e8013c945f455b31900d07ae64
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-08-30 10:47:06 +0600

    Done for Lab final

--- task-management/src/main/java/com/task/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.task.config;
-
-public class JwtConstant {
-    public static final String SECRET_KEY="sgkleajhtionghkghfjjllgytouyjhgkhermbndfsjkgheujfgzxcghrsygerdhrts";
-
-    public static final String JWT_HEADER = "Authorization";
-}
--- task-management/src/main/java/com/task/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task.repository;
-
-import com.task.modal.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByEmail(String email);
-
-}
